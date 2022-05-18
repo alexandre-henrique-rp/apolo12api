cf833f564e4ff33cd130c08284c5641a59d28f7c
Alexandre Henrique da Rocha Araujo <64115785+alexandre-henrique-rp@users.noreply.github.com>
2022-05-18 15:14:23 -0300

    update 18-04

---
+++ models/botscrap.js
@@ -0,0 +1,29 @@
+const puppeteer = require('puppeteer');
+
+
+const botscrap = async (cnpj) => {
+    
+    const browser = await puppeteer.launch({
+        headless: false,
+        defaultViewport: null,
+    });
+    const page = await browser.newPage();
+    await page.goto('https://www.situacao-cadastral.com/');
+    await page.waitFor('input[name="doc"]');
+    await page.type('input[name="doc"]', cnpj, { delay: 155 });
+    await page.keyboard.press('Enter');
+
+    //raspagem
+    await page.waitFor('#resultado');
+    const grip = await page.$eval('div[id="resultado"] > span > i', i => i.innerText);
+    console.log(grip)
+    await browser.close();
+    
+    try {
+        console.log(grip)
+        return grip;
+    } catch (error) {
+        console.log(error)
+    }
+}
+module.exports = botscrap;
